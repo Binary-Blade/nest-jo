ef585a7f1c04c5fbf9e3f1fb66241edcb96283de
Binary-Blade <codegrunt@proton.me>
2024-04-09 22:50:18 +0200

    * feat: Implement the module carts logic

---
+++ src/modules/carts/carts.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { CartsController } from './carts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '@modules/users/entities/user.entity';
+import { Cart } from './entities/cart.entity';
+import { CartItem } from './entities/cartitems.entity';
+import { RedisModule } from '@database/redis/redis.module';
+import { RedisService } from '@database/redis/redis.service';
+import { OffersService } from '@modules/offers/offers.service';
+import { Offer } from '@modules/offers/entities/offer.entity';
+import { CartsService } from './carts.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User, Cart, CartItem, Offer]), RedisModule],
+  controllers: [CartsController],
+  providers: [CartsService, RedisService, OffersService]
+})
+export class CartsModule {}
