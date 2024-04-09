304cee6bf013299a11377416c2e348c7dc4181bf
Binary-Blade <codegrunt@proton.me>
2024-04-10 00:02:36 +0200

    * docs: Add comments to documents methods

--- src/modules/carts/carts.service.ts
+++ src/modules/carts/carts.service.ts
@@ -2,11 +2,22 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Cart } from './entities/cart.entity';
 import { Repository } from 'typeorm';
-
+/**
+ * Service responsible for handling carts.
+ */
 @Injectable()
 export class CartsService {
+  // Inject the Cart repository.
   constructor(@InjectRepository(Cart) private readonly cartRepository: Repository<Cart>) {}
 
+  /**
+   * Finds a cart.
+   *
+   * @param userId The user ID.
+   * @param cartId The cart ID.
+   * @returns The found cart.
+   * @throws NotFoundException if the cart does not exist.
+   **/
   async findCart(userId: number, cartId: number): Promise<Cart> {
     const cart = await this.cartRepository.findOne({
       where: { cartId, user: { userId } }
@@ -15,6 +26,12 @@ export class CartsService {
     return cart;
   }
 
+  /**
+   * Gets or creates a cart for a user.
+   *
+   * @param userId The user ID.
+   * @returns The cart.
+   */
   async getOrCreateCart(userId: number): Promise<Cart> {
     let cart = await this.cartRepository.findOne({
       where: { user: { userId } },
