6ef721532b7ec6b8f606d45dbbb0446d93a6ee20
Binary-Blade <codegrunt@proton.me>
2024-04-14 14:06:07 +0200

    * feat: Create ReservationDTO and add class validator

--- src/modules/reservations/dto/create-reservation.dto.ts
+++ src/modules/reservations/dto/create-reservation.dto.ts
@@ -1,1 +1,19 @@
-export class CreateReservationDto {}
+import { statusReservation } from '@common/enums/status-reservation.enum';
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateReservationDto {
+  @IsNotEmpty()
+  userId: number;
+
+  @IsNotEmpty()
+  cartItemId: number;
+
+  @IsNotEmpty()
+  status: statusReservation;
+
+  @IsNotEmpty()
+  totalPrice: number;
+
+  @IsNotEmpty()
+  paymentId: number;
+}
