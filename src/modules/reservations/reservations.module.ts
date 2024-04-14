2758bb7af3b94bce2bf1ed13f64d3c4c8a515bea
Binary-Blade <codegrunt@proton.me>
2024-04-14 14:06:58 +0200

    * feat: Create method createReservation to iterate using for of from cartItem

--- src/modules/reservations/reservations.module.ts
+++ src/modules/reservations/reservations.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Reservation } from './entities/reservation.entity';
+import { CartItem } from '@modules/cart-items/entities/cartitems.entity';
+import { Cart } from '@modules/carts/entities/cart.entity';
+import { Offer } from '@modules/offers/entities/offer.entity';
+import { User } from '@modules/users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Reservation, CartItem, Cart, Offer, User])],
   controllers: [ReservationsController],
-  providers: [ReservationsService],
+  providers: [ReservationsService]
 })
 export class ReservationsModule {}
--- src/modules/reservations/reservations.service.ts
+++ src/modules/reservations/reservations.service.ts
@@ -1,11 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Reservation } from './entities/reservation.entity';
+import { Repository } from 'typeorm';
+import { User } from '@modules/users/entities/user.entity';
+import { statusReservation } from '@common/enums/status-reservation.enum';
 
 @Injectable()
 export class ReservationsService {
-  create(createReservationDto: CreateReservationDto) {
-    return 'This action adds a new reservation';
+  constructor(
+    @InjectRepository(Reservation) private readonly reservationRepository: Repository<Reservation>,
+    @InjectRepository(User) private readonly userRepository: Repository<User>
+  ) {}
+
+  async createReservations(userId: number, paymentId: number, cartItems: any[]) {
+    const user = await this.userRepository.findOneBy({ userId });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found.`);
+    }
+
+    for (const item of cartItems) {
+      const reservationDto = new CreateReservationDto();
+      reservationDto.userId = userId;
+      reservationDto.paymentId = paymentId;
+      reservationDto.cartItemId = item.cartItemId;
+      reservationDto.status = statusReservation.PENDING;
+      reservationDto.totalPrice = item.offer.price * item.quantity;
+
+      const reservation = this.reservationRepository.create(reservationDto);
+      await this.reservationRepository.save(reservation);
+    }
   }
 
   findAll() {
