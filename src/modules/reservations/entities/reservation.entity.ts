1e819076dd1f7edeb8178786b16c50c303f2bf4d
Binary-Blade <codegrunt@proton.me>
2024-04-14 14:06:24 +0200

    * feat: Upgrade reservation entity

--- src/modules/reservations/entities/reservation.entity.ts
+++ src/modules/reservations/entities/reservation.entity.ts
@@ -1,23 +1,26 @@
 import { statusReservation } from '@common/enums/status-reservation.enum';
 import { CartItem } from '@modules/cart-items/entities/cartitems.entity';
 import { User } from '@modules/users/entities/user.entity';
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany, JoinColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
 
 @Entity('reservations')
 export class Reservation {
   @PrimaryGeneratedColumn('increment')
   reservationId: number;
 
-  @Column({ type: 'varchar', default: statusReservation.PENDING, name: 'type_offer' })
-  status: statusReservation;
-
   @ManyToOne(() => User, user => user.reservations)
   @JoinColumn({ name: 'userId' })
   user: User;
 
   @ManyToOne(() => CartItem, cartItem => cartItem.reservations)
-  @JoinColumn({ name: 'cart_item_id' })
+  @JoinColumn({ name: 'cartItemId' })
   cartItem: CartItem;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2, default: 0 })
+  totalPrice: number;
+
+  @Column({ type: 'varchar', default: statusReservation.PENDING })
+  status: statusReservation;
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   createdAt: Date;
