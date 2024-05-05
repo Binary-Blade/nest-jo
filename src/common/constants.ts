af6bc2a67688b738c2165d64292b5f9e0778071b
Binary-Blade <codegrunt@proton.me>
2024-05-05 06:02:48 +0200

    * feat: Create Prices formula constant to use it

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,12 @@
+import { PriceFormulaEnum } from './enums/price-formula.enum';
+
 export const NODE_ENV = 'NODE_ENV';
 export const DEV_ENV = 'development';
 export const PROD_ENV = 'production';
+
+// Price formula multipliers
+export const PRICES_FORMULA = [
+  { type: PriceFormulaEnum.SOLO, multiplier: 1 },
+  { type: PriceFormulaEnum.DUO, multiplier: 1.3 },
+  { type: PriceFormulaEnum.FAMILY, multiplier: 1.8 }
+];
