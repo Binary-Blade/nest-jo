0437cbdc44c65262a29c0d5874dc04b3f1115a48
Binary-Blade <codegrunt@proton.me>
2024-05-05 06:05:12 +0200

    * feat: Add new providers and typeorm module

--- src/modules/carts/carts.module.ts
+++ src/modules/carts/carts.module.ts
@@ -7,9 +7,12 @@ import { RedisService } from '@database/redis/redis.service';
 import { EventsService } from '@modules/events/events.service';
 import { Event } from '@modules/events/entities/event.entity';
 import { CartsService } from './carts.service';
+import { EventPrice } from '@modules/event-prices/entities/event-price.entity';
+import { EventPricesService } from '@modules/event-prices/event-prices.service';
+import { UtilsService } from '@common/utils/utils.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Cart, Event]), RedisModule],
-  providers: [CartsService, RedisService, EventsService]
+  imports: [TypeOrmModule.forFeature([User, Cart, Event, EventPrice]), RedisModule],
+  providers: [CartsService, RedisService, EventsService, EventPricesService, UtilsService]
 })
 export class CartsModule {}
