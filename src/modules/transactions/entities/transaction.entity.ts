b0e5f995c2ee29abed2c827c563624ed3c7a58e3
Binary-Blade <codegrunt@proton.me>
2024-05-07 14:45:27 +0200

    * feat: Create new transactions modules and features logic for handling payment logic

---
+++ src/modules/transactions/entities/transaction.entity.ts
@@ -0,0 +1,35 @@
+import { StatusReservation } from '@common/enums/status-reservation.enum';
+import { Reservation } from '@modules/reservations/entities/reservation.entity';
+import { User } from '@modules/users/entities/user.entity';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, OneToMany } from 'typeorm';
+
+@Entity('transactions')
+export class Transaction {
+  @PrimaryGeneratedColumn('increment')
+  transactionId: number;
+
+  @ManyToOne(() => User, user => user.transactions)
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @OneToMany(() => Reservation, reservation => reservation.transaction)
+  reservation: Reservation[];
+
+  @Column({ type: 'varchar' })
+  statusPayment: StatusReservation;
+
+  @Column()
+  paymentId: number;
+
+  // @Column()
+  // quantity: number;
+  //
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalAmount: number;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}
---
+++ src/modules/transactions/transactions.service.ts
@@ -0,0 +1,70 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+//import { CreateTransactionDto } from './dto/create-transaction.dto';
+// import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Transaction } from './entities/transaction.entity';
+import { Repository } from 'typeorm';
+import { User } from '@modules/users/entities/user.entity';
+import { PaymentResult } from '@common/interfaces/payment.interface';
+import { CartItem } from '@modules/cart-items/entities/cartitems.entity';
+
+@Injectable()
+export class TransactionsService {
+  constructor(
+    @InjectRepository(Transaction) private transactionRepository: Repository<Transaction>
+  ) {}
+
+  async createTransaction(
+    user: User,
+    total: number,
+    paymentResult: PaymentResult
+  ): Promise<Transaction> {
+    const transaction = this.transactionRepository.create({
+      user,
+      paymentId: Math.floor(Math.random() * 1000000),
+      totalAmount: total,
+      statusPayment: paymentResult.status
+    });
+    return this.transactionRepository.save(transaction);
+  }
+
+  async findTransactionByReservationId(reservationId: number): Promise<Transaction> {
+    const transaction = await this.transactionRepository.findOne({
+      where: { reservation: { reservationId } },
+      relations: ['reservation']
+    });
+
+    if (!transaction) {
+      throw new NotFoundException(`Transaction with reservation ID ${reservationId} not found.`);
+    }
+
+    return transaction;
+  }
+
+  /**
+   * Calculate the total price of a cart
+   *
+   * @private - This method is only used internally by the service
+   * @param cartItems - The items in the cart
+   * @returns - The total price of the cart
+   */
+  calculateCartTotal(cartItems: CartItem[]): number {
+    return cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0);
+  }
+
+  findAll() {
+    return this.transactionRepository.find();
+  }
+  //
+  // findOne(id: number) {
+  //   return `This action returns a #${id} transaction`;
+  // }
+  //
+  // update(id: number, updateTransactionDto: UpdateTransactionDto) {
+  //   return `This action updates a #${id} transaction`;
+  // }
+  //
+  // remove(id: number) {
+  //   return `This action removes a #${id} transaction`;
+  // }
+}
