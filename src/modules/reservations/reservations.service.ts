a67deaa8b733cd8efc01ac83234a30e5dc1aa7fe
Binary-Blade <codegrunt@proton.me>
2024-05-07 17:16:18 +0200

    * feat: Create new class for separate concerns

--- src/modules/reservations/reservations.service.ts
+++ src/modules/reservations/reservations.service.ts
@@ -1,19 +1,8 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from '@modules/users/entities/user.entity';
-import { CartItem } from '@modules/cart-items/entities/cartitems.entity';
-import { UsersService } from '@modules/users/users.service';
-import { CartsService } from '@modules/carts/carts.service';
-import { CartItemsService } from '@modules/cart-items/cart-items.service';
 import { Reservation } from './entities/reservation.entity';
-import { StatusReservation } from '@common/enums/status-reservation.enum';
-import { TicketsService } from '@modules/tickets/tickets.service';
-import { PaymentService } from '@libs/payment/payment.service';
-import { ReservationDetailsService } from '@modules/reservation-details/reservation-details.service';
-import { Transaction } from '@modules/transactions/entities/transaction.entity';
-import { TransactionsService } from '@modules/transactions/transactions.service';
-import { EventsService } from '@modules/events/events.service';
+import { ReservationsProcessorService } from './reservations-processor.service';
 
 /**
  * Service responsible for handling reservations.
@@ -23,123 +12,12 @@ import { EventsService } from '@modules/events/events.service';
 export class ReservationsService {
   constructor(
     @InjectRepository(Reservation) private reservationRepository: Repository<Reservation>,
-    private readonly ticketService: TicketsService,
-    private readonly eventsService: EventsService,
-    private readonly transactionService: TransactionsService,
-    private readonly usersService: UsersService,
-    private readonly cartService: CartsService,
-    private readonly cartItemsService: CartItemsService,
-    private readonly paymentService: PaymentService,
-    private readonly reservationDetailsService: ReservationDetailsService
+    private readonly reservationProcessorService: ReservationsProcessorService
   ) {}
 
-  async processBookingReservations(userId: number, cartId: number): Promise<Reservation[]> {
-    const user = await this.usersService.verifyUserOneBy(userId);
-    const cartItems = await this.cartItemsService.findAllItemsInCart(userId, cartId);
-    if (!cartItems.length) throw new BadRequestException('No items found in the cart.');
-
-    // Verify the cart and calculate the total price
-    const cartTotalPrice = this.transactionService.calculateCartTotal(cartItems);
-    // Process the payment
-    const paymentResult = await this.paymentService.processPayment(cartTotalPrice);
-
-    // Create a transaction for the payment
-    const transaction = await this.transactionService.createTransaction(
-      user,
-      cartTotalPrice,
-      paymentResult
-    );
-    let reservations = await this.processCartItems(cartItems, user, transaction);
-    if (paymentResult.status === StatusReservation.APPROVED) {
-      await this.finalizeBooking(cartItems, reservations);
-    }
-    await this.cleanUpAfterPayment(cartId, userId);
-    return reservations;
+  async generateReservation(userId: number, cartId: number): Promise<Reservation[]> {
+    return await this.reservationProcessorService.processUserReservation(userId, cartId);
   }
-
-  private async processCartItems(
-    cartItems: CartItem[],
-    user: User,
-    transaction: Transaction
-  ): Promise<Reservation[]> {
-    let reservations = [];
-    for (const item of cartItems) {
-      reservations.push(...(await this.createReservationsForItem(item, user, transaction)));
-    }
-    return reservations;
-  }
-
-  private async createReservationsForItem(
-    item: CartItem,
-    user: User,
-    transaction: Transaction
-  ): Promise<Reservation[]> {
-    let reservations = [];
-    for (let i = 0; i < item.quantity; i++) {
-      let reservation = await this.createReservation(user, item, transaction);
-      const reservationDetail =
-        await this.reservationDetailsService.createReservationDetailsFromReservation(
-          reservation,
-          item
-        );
-      reservation.reservationDetails = reservationDetail;
-      reservation = await this.reservationRepository.save(reservation);
-      reservations.push(reservation);
-    }
-    return reservations;
-  }
-  private async createReservation(
-    user: User,
-    cartItem: CartItem,
-    transaction: Transaction
-  ): Promise<Reservation> {
-    const reservation = this.reservationRepository.create({
-      user,
-      cartItem,
-      transaction
-    });
-    return this.reservationRepository.save(reservation);
-  }
-
-  private async finalizeBooking(cartItems: CartItem[], reservations: Reservation[]): Promise<void> {
-    const eventIds = new Set(cartItems.map(item => item.event.eventId));
-    for (const eventId of eventIds) {
-      const itemsForEvent = cartItems.filter(item => item.event.eventId === eventId);
-      await this.eventsService.processEventTicketsAndRevenue(itemsForEvent);
-    }
-    await this.ticketService.generateTicketsForApprovedReservations(reservations);
-  }
-  /**
-   * Clean up after a payment
-   *
-   * @private - This method is only used internally by the service
-   * @param cartId - The ID of the cart to clean up
-   * @param userId - The ID of the user to clean up
-   * @returns - A promise that resolves when the cleanup is complete
-   * @throws Error if the cleanup fails
-   */
-  private async cleanUpAfterPayment(cartId: number, userId: number) {
-    await this.cartItemsService.removeAllItemFromCart(userId, cartId);
-    await this.cartService.deleteCart(cartId);
-  }
-
-  /**
-   * Ensure that a reservation does not already exist for an item
-   *
-   * @private - This method is only used internally by the service
-   * @param item - The item to check for existing reservations
-   * @param user - The user making the reservation
-   * @throws Error if a reservation already exists for the item
-   * @returns - A promise that resolves if there are no duplicate reservations
-   */
-  // private async ensureNoDuplicateReservation(item: CartItem, user: User): Promise<void> {
-  //   const existingReservation = await this.reservationRepository.findOne({
-  //     where: { cartItem: item, user }
-  //   });
-  //   if (existingReservation) {
-  //     throw new Error(`Reservation already exists for item with ID ${item.cartItemId}.`);
-  //   }
-  // }
 
   /**
    * Find all reservations for a user
