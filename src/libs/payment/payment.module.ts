87297660bc457a6257f78aa738852dfa1672d9aa
Binary-Blade <codegrunt@proton.me>
2024-05-07 14:43:46 +0200

    * refactor: Add service providers and import modules

--- src/libs/payment/payment.module.ts
+++ src/libs/payment/payment.module.ts
@@ -15,25 +15,44 @@ import { Ticket } from '@modules/tickets/entities/ticket.entity';
 import { TicketsService } from '@modules/tickets/tickets.service';
 import { EventPrice } from '@modules/events/entities/event-price.entity';
 import { EventPricesService } from '@modules/events/event-prices.service';
-import { OrdersService } from '@modules/orders/orders.service';
-import { Order } from '@modules/orders/entities/order.entity';
+import { ReservationDetails } from '@modules/reservation-details/entities/reservation-details.entity';
+import { ReservationDetailsService } from '@modules/reservation-details/reservation-details.service';
+import { Transaction } from '@modules/transactions/entities/transaction.entity';
+import { TransactionsService } from '@modules/transactions/transactions.service';
+import { EventsService } from '@modules/events/events.service';
+import { RedisModule } from '@database/redis/redis.module';
+import { UtilsService } from '@common/utils/utils.service';
 
 /**
  * Module for handling payments.
  */
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Reservation, CartItem, Event, Cart, User, Ticket, EventPrice, Order])
+    TypeOrmModule.forFeature([
+      Reservation,
+      CartItem,
+      Event,
+      Cart,
+      User,
+      Ticket,
+      EventPrice,
+      ReservationDetails,
+      Transaction
+    ]),
+    RedisModule
   ],
   providers: [
     PaymentService,
+    TransactionsService,
     ReservationsService,
     CartItemsService,
+    EventsService,
+    UtilsService,
     CartsService,
     UsersService,
     EncryptionService,
     TicketsService,
-    OrdersService,
+    ReservationDetailsService,
     EventPricesService
   ]
 })
--- src/libs/payment/payment.service.ts
+++ src/libs/payment/payment.service.ts
@@ -8,8 +8,7 @@ import { StatusReservation } from '@common/enums/status-reservation.enum';
  */
 @Injectable()
 export class PaymentService {
-  private readonly SUCCESS_RATE = 0.6; // 60% chance of payment being successful
-  private readonly PENDING_RATE = 0.2; // 20% chance of payment being pending
+  private readonly SUCCESS_RATE = 0.8; // 60% chance of payment being successful
 
   /**
    * Processes a payment for a reservation.
@@ -31,11 +30,6 @@ export class PaymentService {
       return {
         status: StatusReservation.APPROVED,
         detail: 'Payment processed successfully.'
-      };
-    } else if (random < this.PENDING_RATE + this.SUCCESS_RATE) {
-      return {
-        status: StatusReservation.PENDING,
-        detail: 'Payment is pending confirmation.'
       };
     } else {
       return {
