615d246a9bc5a74eb08562e879425a11a3cb1f49
Binary-Blade <codegrunt@proton.me>
2024-05-13 12:10:29 +0200

    * doc: Delete unused comments

--- src/modules/cart-items/cart-items.controller.ts
+++ src/modules/cart-items/cart-items.controller.ts
@@ -5,10 +5,6 @@ import { UpdateCartItemDto } from './dto/update-cart-item.dto';
 import { AccessTokenGuard } from '@security/guards';
 import { CartItemsService } from './cart-items.service';
 
-//  TODO:
-// SEE HOW TO IMPLEMENT THE LOGIC WITH FRONTEND FOR LOCALSTORAGE
-// BECAUSE CLIENT CAN ADD ITEMS TO CART WITHOUT BEING LOGGED IN BUT WHEN IT LOGS IN IT SHOULD BE ABLE TO RETRIEVE THE CART ITEMS
-
 /**
  * Controller responsible for handling requests to the /carts route
  * This controller is used to manage cart items, including adding, updating, and removing items from a cart.
