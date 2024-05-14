70f0fc7710773d54dec094e1bf4dabb9f6fc33ca
Binary-Blade <codegrunt@proton.me>
2024-05-14 17:34:02 +0200

    * feat: Add new class validator

--- src/modules/cart-items/dto/create-cart-item.dto.ts
+++ src/modules/cart-items/dto/create-cart-item.dto.ts
@@ -1,17 +1,26 @@
 import { PriceFormulaEnum } from '@common/enums/price-formula.enum';
-import { IsEnum, IsInt, IsNotEmpty, Min } from 'class-validator';
+import { IsEnum, IsInt, IsNotEmpty, IsPositive, Min } from 'class-validator';
 
 export class CreateCartItemDto {
+  @IsNotEmpty()
+  @IsInt()
+  readonly userId: number;
+
   @IsNotEmpty()
   @IsInt()
   readonly eventId: number;
 
   @IsNotEmpty()
   @IsInt()
+  @IsPositive()
   @Min(1)
   readonly quantity: number;
 
   @IsNotEmpty()
   @IsEnum(PriceFormulaEnum)
   readonly priceFormula: PriceFormulaEnum;
+
+  @IsNotEmpty()
+  @IsInt()
+  price: number;
 }
