ed3fb1fbd344bcc6c43dc931e143faa3e5470799
Binary-Blade <codegrunt@proton.me>
2024-05-21 03:11:00 +0200

    * fix: Fixing for deployment

--- src/database/migrations/1712751776642-CreateTableReservations.ts
+++ src/database/migrations/1712751776642-CreateTableReservations.ts
@@ -18,7 +18,6 @@ export class CreateTableReservations1712751776642 implements MigrationInterface 
                     "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                     "updatedAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                     FOREIGN KEY ("userId") REFERENCES "users" ("userId") ON DELETE SET NULL,
-                    FOREIGN KEY ("transactionId") REFERENCES "transactions" ("transactionId") ON DELETE SET NULL,
                     FOREIGN KEY ("cartItemId") REFERENCES "cart_items" ("cartItemId") ON DELETE SET NULL
                 );
             `);
